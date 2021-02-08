9d7ffbe3e0bab62156e0c9cdef82f32577817493
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-08 02:49:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
+     const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+     for (let idx = 0; idx < newCollection.length; idx++)
+       iteratee(newCollection[idx])
+
+     return collection
     },
 
-    map: function() {
+    map: function(collection, iteratee) {
+     if (!(collection instanceof Array))
+       collection = Object.values(collection)
 
-    },
+     const newArr = []
+
+     for (let idx = 0; idx < collection.length; idx++)
+       newArr.push(iteratee(collection[idx]))
+
+     return newArr
+   },
 
     reduce: function() {
 
